d29b9b1aebe16bdca10c8c9a40aa4ca5fbf3106a
github username <github email address>
2018-05-30 17:56:03 -0400

    second commit play game

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,16 @@
 require_relative '../config/environment'
 require "timeout"
+require 'terminal-table'
 
 
+menu
+# play_game
 
-binding.pry
-play_game
+# Game.all.sort {|x,y| y[:score] <=>x[:score]}
+# name= Game.all.sort {|x,y| y[:score] <=>x[:score]}[4].user.name
+# score=Game.all.sort {|x,y| y[:score] <=>x[:score]}[4].score
+# Game.all.sort {|x,y| y[:score] <=>x[:score]}[4].word.word
+
+
+# binding.pry
+# true
--- lib/play.rb
+++ lib/play.rb
@@ -3,6 +3,8 @@ require "timeout"
 require_relative '../app/models/user.rb'
 require_relative '../app/models/word.rb'
 require_relative '../app/models/game.rb'
+require "tty-prompt"
+require 'pry'
 
 
 def create_random_letters
@@ -37,17 +39,31 @@ def get_anagram_array_from_api
   api_return_array
 end
 
+def menu
+  prompt = TTY::Prompt.new
+  user_select = prompt.select("Please select your choice?", %w(Play Scoreboard Quit))
+  if user_select == "Play"
+    play_game
+  elsif user_select == "Scoreboard"
+    scorecard
+  elsif user_select == "Quit"
+    puts "Bye!"
+  end
+end
+
 def play_game
-  puts "Please enter your username"
-  name = gets.chomp
+    puts "Please enter your first name"
+  first_name = gets.chomp
+    puts "Please enter your last name"
+  last_name = gets.chomp
+  name = first_name + last_name
   user = User.create(name: name)
+
   anagram_array = []
     until anagram_array.any? == true
       anagram_array = get_anagram_array_from_api
     end
-
   score = 0
-
   begin
     Timeout::timeout 20 do
     loop do
@@ -66,4 +82,24 @@ def play_game
      puts "Sorry,you are out of time! You got #{score} points!"
   end
   Game.create(user: user, word: $word, score: score)
+  play_again
+end
+
+def play_again
+  prompt = TTY::Prompt.new
+  ans = prompt.yes?("Do you want to play again?")
+  if ans == true
+    play_game
+  else 
+    puts "Bye!"
+  end
+end
+
+def scorecard
+rows=[]
+  Game.all.sort {|x,y| y[:score] <=>x[:score]}.each do |game|
+    rows << [game.user.name, game.word.word, game.score]
+  end
+  table=Terminal::Table.new :title =>"Scoreboard", :headings => ["Username","Letters","Score"], :rows => rows, :style =>{:width => 50, :padding_left => 3, :border_x => "=", :border_i => "x"}
+  puts table
 end
